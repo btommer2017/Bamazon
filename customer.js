3356f305c7f3a2cc2d0dee33bff9518f483f5987
btommer2017 <btommer@gmail.com>
2018-05-07 07:22:08 -0500

    added receipt function and clear results for next customer

--- customer.js
+++ customer.js
@@ -4,147 +4,165 @@ require("console.table");
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "bamazonDB"
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "bamazonDB"
 });
 
 // connect to the mysql server and sql database
 connection.connect(function (err) {
-  if (err) throw err;
+    if (err) throw err;
 
-  // run the start function after the connection is made to prompt the user
-  queryUser();
+    // promt the user after the connection is made
+    queryUser();
 });
 
 function listItems() {
-  connection.query("SELECT * FROM products", function (err, res) {
-    if (err) throw err;
-    console.table(res);
-    queryUser();
-    // userPurchase();
-  });
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        queryUser();
+    });
 }
 
 function queryUser() {
-  inquirer
-    .prompt([{
-      name: "query",
-      type: "list",
-      message: "Please choose from the following options",
-      choices: ["See Inventory", "Purchase products", "Exit"]
-    }]).then(function (inquirerResponse) {
+    inquirer
+        .prompt([{
+            name: "query",
+            type: "list",
+            message: "Please choose from the following options",
+            choices: ["Purchase products", "See Inventory", "Exit"]
+        }]).then(function (inquirerResponse) {
 
-      switch (inquirerResponse.query) {
+            switch (inquirerResponse.query) {
 
-        case 'See Inventory':
-          listItems();
-          break;
+                case 'See Inventory':
+                    listItems();
+                    break;
 
-        case 'Purchase products':
-          userPurchase();
-          break;
+                case 'Purchase products':
+                    userPurchase();
+                    break;
 
-        case 'Exit':
-          exitStore();
-          break;
-      }
-    });
+                case 'Exit':
+                    exitStore();
+                    break;
+            }
+        });
 }
 
 function userPurchase() {
 
-  connection.query("SELECT * FROM products", function (err, res) {
-    if (err) throw err;
-    inquirer
-      .prompt([{
-        name: "id",
-        type: "input",
-        message: "Please input the item's ID you would like to purchase.",
-        choices: console.table(res),
-        validate: function (value) {
-          var numL = res.length;
-          if (value <= numL && value > 0 && res[value - 1].stock_quantity > 0) {
-            console.log("\n\nProduct: " + res[value - 1].product_name + "\nPrice: $" + res[value - 1].price + "\nQuantity Available: " + res[value - 1].stock_quantity + "\n");
-            return true;
-          }
-          console.log("\n\nThe ID you chose is either out of stock, or invalid, please make another selection.\n");
-
-          //  console.log(res[value].stock_quantity + " current quantity");
-          return false;
-
-        }
-      }])
-      .then(function (inquirerResponse) {
-        var num = (inquirerResponse.id - 1);
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
         inquirer
-          .prompt([{
-            name: "quantity",
-            type: "input",
-            message: "How many would you like to purchase?",
-            validate: function (value) {
-              stockQuan = res[num].stock_quantity;
+            .prompt([{
+                name: "id",
+                type: "input",
+                message: "Please input the item's ID you would like to purchase.",
+                choices: console.table(res),
+                validate: function (value) {
+                    var numL = res.length;
+                    if (value <= numL && value > 0 && res[value - 1].stock_quantity > 0) {
+                        console.log("\n\nProduct: " + res[value - 1].product_name + "\nPrice: $" + res[value - 1].price + "\nQuantity Available: " + res[value - 1].stock_quantity + "\n");
+                        return true;
+                    }
+                    console.log("\n\nThe ID you have chosen is either out of stock, or invalid. Please make another selection.\n");
 
-              if ((value <= stockQuan) && (value > 0)) {
-                return true;
-              }
-              console.log("\n\nPlease enter a valid amount.\n");
-              return false;
-            }
+                    return false;
 
-          }])
-
-          .then(function (inquirerResponse) {
-            var quan = (inquirerResponse.quantity);
-            var each = res[num].price;
-            var cost = quan * each;
-            console.log(`\nItem: ${res[num].product_name}`);
-            console.log(`Quantity: ${quan}`);
-            console.log(`Cost per item: $${each}`);
-            console.log(`Total Cost: $${cost}\n`);
-            connection.query(
-              "UPDATE products SET ? WHERE ?", [{
-                  stock_quantity: (stockQuan - quan)
-                },
-                {
-                  id: (num + 1)
                 }
-              ],
-              function (error) {
-                if (error) throw err;
-              }
-            );
+            }])
+            .then(function (inquirerResponse) {
+                var num = (inquirerResponse.id - 1);
+                inquirer
+                    .prompt([{
+                        name: "quantity",
+                        type: "input",
+                        message: "How many would you like to purchase?",
+                        validate: function (value) {
+                            stockQuan = res[num].stock_quantity;
 
-            keepShopping();
+                            if ((value <= stockQuan) && (value > 0)) {
+                                return true;
+                            }
+                            console.log("\n\nPlease enter a valid amount.\n");
+                            return false;
+                        }
 
-          })
-      })
-  })
+                    }])
+
+                    .then(function (inquirerResponse) {
+                        quan = (inquirerResponse.quantity);
+                        each = res[num].price;
+                        cost = quan * each;
+                        console.log(`\nItem: ${res[num].product_name}`);
+                        console.log(`Quantity: ${quan}`);
+                        console.log(`Cost per item: $${each}`);
+                        console.log(`Total Cost: $${cost}\n`);
+                        connection.query(
+                            "UPDATE products SET ? WHERE ?", [{
+                                    stock_quantity: (stockQuan - quan)
+                                },
+                                {
+                                    id: (num + 1)
+                                }
+                            ],
+
+                            function (error) {
+                                if (error) throw err;
+                            }
+                        );
+
+                        connection.query("INSERT INTO shopping_cart SET ?", {
+                            product_name: res[num].product_name,
+                            price: each,
+                            quantity: quan,
+                            total: (quan * each)
+                        }, )
+
+                        keepShopping();
+
+                    })
+            })
+    })
 }
 
 function keepShopping() {
-  inquirer
-    .prompt([{
-      name: "continue",
-      type: "list",
-      message: "Anything else we can help you with?",
-      choices: ["Keep shopping", "Exit"]
-    }])
-    .then(function (inquirerResponse) {
+    inquirer
+        .prompt([{
+            name: "continue",
+            type: "list",
+            message: "Anything else we can help you with?",
+            choices: ["Keep shopping", "Exit"]
+        }])
+        .then(function (inquirerResponse) {
 
-      if (inquirerResponse.continue === "Keep shopping") {
+            if (inquirerResponse.continue === "Keep shopping") {
 
-        queryUser();
-      } else {
-        console.log("");
-        exitStore();
-      }
-    });
+                queryUser();
+            } else {
+                console.log("");
+                exitStore();
+            }
+        });
 }
 
 function exitStore() {
-  console.log("\nThank you for shopping!");
-  connection.end();
+    console.log("\nThank you for shopping!");
+    console.log("Here's your receipt:");
+
+    connection.query("SELECT * FROM shopping_cart", function (err, res) {
+        if (err) throw err;
+        console.table(res);
+    });
+
+    // Clear the order for the next customer
+    connection.query("TRUNCATE TABLE shopping_cart", function (err, res) {
+        if (err) throw err;
+    });
+
+    connection.end();
 }
