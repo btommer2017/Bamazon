1bc811e727542360144ae83030219199b1ee6607
btommer2017 <btommer@gmail.com>
2018-05-07 08:06:13 -0500

    Updated mysql code in bamazon.sql

--- bamazon.sql
+++ bamazon.sql
@@ -7,45 +7,45 @@ USE bamazonDB;
 CREATE TABLE shopping_cart(
  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   product_name VARCHAR(45) NULL,
-  price DECIMAL(10,2) NULL,
+  price NUMERIC(10,2) NULL,
   quantity INTEGER(10) NULL,
-  total DECIMAL(10,2) NULL
+  total NUMERIC(10,2) NULL
 );
 
 CREATE TABLE products(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   product_name VARCHAR(45) NULL,
   department_name VARCHAR(45) NULL,
-  price DECIMAL(10,2) NULL,
+  price NUMERIC(10,2) NULL,
   stock_quantity INTEGER(10) NULL
 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Wiffle Ball Bat", "Sports", 5.00, 100 );
+VALUES ("Wiffle Ball Bat", "Sports", 4.99, 100 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Wiffle Ball", "Sports", 1.00, 100 );
+VALUES ("Wiffle Ball", "Sports", 1.99, 100 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Nerf Football", "Sports", 5.00, 50 );
+VALUES ("Nerf Football", "Sports", 4.99, 50 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Official NFL Football", "Sports", 60.00, 25 );
+VALUES ("Official NFL Football", "Sports", 59.99, 25 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Baseball Cap", "Clothing", 15.00, 30 );
+VALUES ("Baseball Cap", "Clothing", 14.99, 30 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Basketball Shoes One Size Fits All", "Clothing", 100.00, 150 );
+VALUES ("Basketball Shoes One Size Fits All", "Clothing", 99.99, 150 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Plain Yellow T-shirt xl", "Clothing", 10.00, 30 );
+VALUES ("Plain Yellow T-shirt xl", "Clothing", 9.99, 30 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Plain Yellow T-shirt md", "Clothing", 10.00, 30 );
+VALUES ("Plain Yellow T-shirt md", "Clothing", 9.99, 30 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Plain Yellow T-shirt sm", "Clothing", 10.00, 30 );
+VALUES ("Plain Yellow T-shirt sm", "Clothing", 9.99, 30 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Green Turtleneck xl", "Clothing", 12.50, 20 );
+VALUES ("Green Turtleneck xl", "Clothing", 11.99, 20 );
