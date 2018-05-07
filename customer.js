c39a9abe3441767f6ff73a615976f8da6452b6a6
btommer2017 <btommer@gmail.com>
2018-05-06 19:03:39 -0500

    Code works for purchasing, need to add some more functions to check vs stock quantity and reduce quantity after purchase

--- customer.js
+++ customer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+require("console.table");
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
@@ -19,37 +20,83 @@ connection.connect(function(err) {
   if (err) throw err;
    
   // run the start function after the connection is made to prompt the user
-  listItems();
+  queryUser();
 });
 
 
 function listItems(){
     connection.query("SELECT id, product_name, price FROM products", function(err, res) {
       if (err) throw err;
-      console.log(res);
-      userPurchase();
+      console.table(res);
+      queryUser();
+      // userPurchase();
     });
   }
   
+function queryUser(){
+  inquirer
+    .prompt([
+      {
+name: "query",
+type: "list",
+message: "Please choose from the following options",
+choices: ["See Inventory", "Purchase products", "Exit" ]
+      }
+    ]).then(function(inquirerResponse){
+console.log(inquirerResponse.query);
+
+	switch(inquirerResponse.query){
+
+        // 
+        case 'See Inventory':
+		listItems();
+		break;
+
+        // 
+		case 'Purchase products':
+		userPurchase();
+		break;
+
+        // 
+		case 'Exit':
+		exitStore();
+		break;
+  
+		
+	}
+});
+
+
+    }
+
+
+
+
 function userPurchase(){
 
   connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
-
+    console.log("These are the results before user input and no table");
+console.log(res[0].id);
+console.log(res[0].product_name);
+console.log(res[0].price);
+console.log(res[0].stock_quantity);
+console.log("###################################################################################3");
     inquirer
     .prompt([
          {
-        name: "purchase",
-        type: "list",
+        name: "id",
+        type: "input",
         message: "Please select the item's ID you would like to purchase.",
 
-        choices: function(){
-          var choiceArray = [];
-          for (var i = 0; i < res.length; i++){
-            choiceArray.push(res[i].product_name);
-          }
-          return choiceArray;
-        }
+        // choices: function(){
+        //   var choiceArray = [];
+        //   for (var i = 0; i < res.length; i++){
+        //     choiceArray.push(res[i].product_name);
+        //   }
+        //   return choiceArray;
+        // }
+        choices: console.table(res)
 
       },
       {
@@ -61,15 +108,31 @@ function userPurchase(){
   
       .then(function(inquirerResponse) {
 //   if(inquirerResponse.l)
-console.log(inquirerResponse.purchase);
-console.log(inquirerResponse.quantity);
-console.log("so far stuff is sorta working");
+var num = (inquirerResponse.id-1);
+var quan = (inquirerResponse.quantity);
+var cost = quan * res[num].price
+// console.log(num);
+// console.log(inquirerResponse.id);
+// console.log(inquirerResponse.quantity);
+console.log(`Item: ${res[num].product_name}, Quantity: ${quan}. Total Cost: $${cost}  `);
+console.log(`Item: ${res[num].product_name}`);
+console.log(`Quantity: ${quan}`);
+console.log(`Total Cost: $${cost}`);
+// console.log(inquirerResponse.purchase);
+// console.log(inquirerResponse.quantity);
+// console.log("so far stuff is sorta working");
 connection.end();
 
 })
   })
 }
 
+
+function exitStore(){
+console.log("Thank you for shopping!");
+connection.end();
+
+}
 
 
 
